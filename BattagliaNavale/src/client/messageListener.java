2b8981612fe7a1249fa571aa5db0322069dbcf83
giamberinigiulia <56107097+giamberinigiulia@users.noreply.github.com>
2019-12-02 23:06:11 +0100

    Update messageListener.java with wav's songs effect

--- BattagliaNavale/src/client/messageListener.java
+++ BattagliaNavale/src/client/messageListener.java
@@ -97,13 +97,26 @@ public class messageListener implements Runnable
                     client.drawPiece(Integer.parseInt(command[1]), Integer.parseInt(command[2]));
                     break;
                 case "HIT": //Hit
+                    {
+                        System.out.println("I hit");
+                        try {
+                            Music Hit = new Music(new BufferedInputStream(getClass().getResourceAsStream("/resources/sounds/HitSound.wav")),2000);
+                            Hit.run();
+                        } catch (LineUnavailableException ex) {
+                            Logger.getLogger(messageListener.class.getName()).log(Level.SEVERE, null, ex);
+                        } catch (UnsupportedAudioFileException ex) {
+                            Logger.getLogger(messageListener.class.getName()).log(Level.SEVERE, null, ex);
+                        } catch (IOException ex) {
+                            Logger.getLogger(messageListener.class.getName()).log(Level.SEVERE, null, ex);
+                        }
+                    }
                     client.drawHit(Integer.parseInt(command[1]), Integer.parseInt(command[2]));
                     break;
                 case "WAT": //Water
                     client.drawWater(Integer.parseInt(command[1]), Integer.parseInt(command[2]));
                     {
                         try {
-                            Music Water = new Music(new BufferedInputStream(getClass().getResourceAsStream("/resources/sounds/WaterSound.mp3")),2000);
+                            Music Water = new Music(new BufferedInputStream(getClass().getResourceAsStream("/resources/sounds/WaterSound.wav")),2000);
                             Water.run();
                         } catch (LineUnavailableException ex) {
                             Logger.getLogger(messageListener.class.getName()).log(Level.SEVERE, null, ex);
@@ -131,20 +144,33 @@ public class messageListener implements Runnable
                     break;
                 case "WIN":
                     client.setText("Hai vinto!");
-//                    {
-//                        try {
-//                            Music Win = new Music(new File(getClass().getResource("VictorySoundEffect.wav").toString()),8000);
-//                        } catch (LineUnavailableException ex) {
-//                            Logger.getLogger(messageListener.class.getName()).log(Level.SEVERE, null, ex);
-//                        } catch (UnsupportedAudioFileException ex) {
-//                            Logger.getLogger(messageListener.class.getName()).log(Level.SEVERE, null, ex);
-//                        } catch (IOException ex) {
-//                            Logger.getLogger(messageListener.class.getName()).log(Level.SEVERE, null, ex);
-//                        }
-//                    }
+                  {
+                        try {
+                            Music Win = new Music(new BufferedInputStream(getClass().getResourceAsStream("/resources/sounds/VictorySoundEffect.wav")),2000);
+                            Win.run();
+                        } catch (LineUnavailableException ex) {
+                            Logger.getLogger(messageListener.class.getName()).log(Level.SEVERE, null, ex);
+                        } catch (UnsupportedAudioFileException ex) {
+                            Logger.getLogger(messageListener.class.getName()).log(Level.SEVERE, null, ex);
+                        } catch (IOException ex) {
+                            Logger.getLogger(messageListener.class.getName()).log(Level.SEVERE, null, ex);
+                        }
+                    }
                     break;
                 case "LOS":
                     client.setText("Oh no! Hai perso!");
+                    {
+                        try {
+                            Music Lose = new Music(new BufferedInputStream(getClass().getResourceAsStream("/resources/sounds/LoseSound.wav")),2000);
+                            Lose.run();
+                        } catch (LineUnavailableException ex) {
+                            Logger.getLogger(messageListener.class.getName()).log(Level.SEVERE, null, ex);
+                        } catch (UnsupportedAudioFileException ex) {
+                            Logger.getLogger(messageListener.class.getName()).log(Level.SEVERE, null, ex);
+                        } catch (IOException ex) {
+                            Logger.getLogger(messageListener.class.getName()).log(Level.SEVERE, null, ex);
+                        }
+                    }
                     break;
                 case "SNH": //Ships Not Hhit
                     client.drawNotHit(Integer.parseInt(command[1]), Integer.parseInt(command[2]));
